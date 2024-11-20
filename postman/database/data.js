74f5e42fbec3ec06c2857c27e1eb0f785c7fc155
RohitPrakash27 <157983790+RohitPrakash27@users.noreply.github.com>
2024-11-20 23:42:38 +0530

    Create data.js

---
+++ postman/database/data.js
@@ -0,0 +1,6 @@
+const placements =[
+    {id:1,name:'Rohit', company:'Mr. Cooper', branch:'IT'},
+    {id:2, name:'Prakash', company: 'ZOHO', branch: 'IT'},
+    {id:3, name:'Sujeeth',company:'Fidelity', branch:'IT'}];
+
+module.exports = placements;
